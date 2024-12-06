6cacba8617d7a2bd06d1db9d750055902253543d
Suprotiv <suprotiv3@gmail.com>
2024-12-06 19:59:08 +0530

    new updates

--- src/pages/Servicepage.jsx
+++ src/pages/Servicepage.jsx
@@ -11,35 +11,35 @@ const Servicepage = () => {
       title: 'Short Form Content',
       description: 'Engaging short form reels, shorts, and video content optimized for platforms like Instagram, Youtube and TikTok that will help you grow your page as I utilize video editing strategies focused on maximizing viewer retention and engagement',
       icon: faFilm,
-      backgroundImage: '/thumbnail_2.png',
+      backgroundImage: '/thumbnail_2.webp',
       orientation: 'left' // Image on the left
     },
     {
       title: 'Commercial Advertisements',
       description: 'Professional video content tailored for your brand, including promotional videos, testimonials, and more. I will provide videos that reflect a consistent style and align with your brand’s unique identity, perfectly suited for your brand’s website, social media or any other platform',
       icon: faBullhorn,
-      backgroundImage: '/thumbnail_6.png',
+      backgroundImage: '/thumbnail_6.webp',
       orientation: 'right' // Image on the right
     },
     {
       title: 'Long Form Content',
       description: 'Long form editing packages designed for content creators, including thumbnails and optimized SEO for your Youtube and other video hosting platforms',
       icon: faVideo,
-      backgroundImage: '/lfc.png',
+      backgroundImage: '/lfc.webp',
       orientation: 'left'
     },
     {
       title: 'VFX',
       description: 'Special effects, green screen keying, compositing, and 3D integrations for creating powerful, cinematic visuals that captivate audiences and enhance storytelling. Whether adding realistic elements or transforming backgrounds, I bring creativity and technical precision to achieve high-impact effects to your videos',
       icon: faMagic,
-      backgroundImage: '/vfx.png',
+      backgroundImage: '/vfx.webp',
       orientation: 'right'
     },
     {
       title: 'Audio Design',
       description: 'Professional sound design, audio mixing, and syncing to enhance the storytelling experience. From clean dialogue and atmospheric effects to dynamic soundscapes, I ensure that every project sounds as polished as it looks, bringing depth and emotion to the visuals.',
       icon: faVolumeUp,
-      backgroundImage: '/audio.png',
+      backgroundImage: '/audio.webp',
       orientation: 'left'
     }
   ];
@@ -48,7 +48,7 @@ const Servicepage = () => {
     <>
   <div className="relative w-full h-[50vh] md:h-[80vh] animate-fadeIn">
   {/* Background Image */}
-  <img src="background.png" className="object-cover w-full h-full object-center" alt="Background" />
+  <img src="background.webp" className="object-cover w-full h-full object-center" alt="Background" />
 
   {/* Centered Text */}
   <div className="absolute inset-0 flex flex-col justify-center items-center p-4 bg-black bg-opacity-40">
