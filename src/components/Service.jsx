5998ab670de8f2dfba5fc31ed82dba21a217deee
Suprotiv <suprotiv3@gmail.com>
2024-12-10 17:49:15 +0530

    changes

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,25 +2,44 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import useScrollAnimation from './useScrollAnimation';
+import LazyLoader from './LazyLoader';
 
 const Service = ({ title, description, icon, backgroundImage, orientation }) => {
   useScrollAnimation()
   return (
     <div className='overflow-hidden'>
+      <LazyLoader/>
     <div 
       className={`flex flex-col md:flex-row gap-10    items-center px-6 py-12 rounded-lg  text-white  ${
         orientation === 'right' ? 'md:flex-row-reverse animate-on-scroll1 ' : 'animate-on-scroll'
       }`}
     >
-      {/* Image */}
-      <div className={`w-full md:w-1/2 flex-shrink-0 mb-6 md:mb-0 `}>
-        <img 
-          src={backgroundImage} 
-          alt={`${title} image`}
-          className="rounded-lg object-cover h-[50vh]  w-full"
-          loading="lazy"
-        />
-      </div>
+  <div 
+  className="blur-load relative rounded-md" // Ensure the container is relative
+  style={{
+    backgroundImage: `url(lowres/${backgroundImage.slice(0, -5)}_lowres.jpg)`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: '50%', // Ensures the container takes the full height
+    height: '100%', // Sets the background beneath
+  }}
+>
+  {/* Image */}
+  <div 
+    className="w-full  flex-shrink-0 mb-6 md:mb-0 relative z-10" // Image is positioned above
+  >
+    <img 
+      src={backgroundImage} 
+      alt={`${title} image`}
+      className="rounded-lg object-cover h-[55vh] w-full"
+      loading="lazy"
+    />
+  </div>
+</div>
+
+
+
       
       {/* Text content */}
       <div className="p-4 w-full md:w-1/2 text-center md:text-left">
