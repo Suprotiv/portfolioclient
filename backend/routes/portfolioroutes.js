fd850202bf10f64b3bdb788d9a6539c2260b1348
Suprotiv <suprotiv3@gmail.com>
2024-12-12 00:36:26 +0530

    changes

--- backend/routes/portfolioroutes.js
+++ backend/routes/portfolioroutes.js
@@ -34,7 +34,7 @@ router.get('/getprojects', async (req,res)=>{
     res.json(projects)
 
 })
-router.get('/getclients', async (req,res)=>{
+router.get('/getclient', async (req,res)=>{
     
     const projects= await ClientModel.find({})
 
