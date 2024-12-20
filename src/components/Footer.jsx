1450d6fdaa75c146ffd7b52c14863330715c269f
Suprotiv <suprotiv3@gmail.com>
2024-12-20 19:09:22 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white pt-[10vh] pb-[5vh]">
+    <footer className="bg-black text-white  pt-[5vh] md:pt-[10vh] pb-[5vh]">
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Social Media Icons */}
         <div className="flex justify-center space-x-6 mb-6">
